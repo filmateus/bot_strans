380eaeb95a3bb7b8aa953a334dc547eceefa6b89
filmateus <filipemateus@outlook.com>
2022-11-15 04:39:46 -0300

    update search

--- code_sql.sql
+++ code_sql.sql
@@ -1,3 +1,4 @@
+-- create a database
 CREATE DATABASE BD_STRANS;
 GO
 
@@ -44,7 +45,7 @@ GO
 
 --- we have problems in table matriculas with duplicated rows, it's importante delete this rows
 --- find duplicates elements
-SELECT COD_MATRICULA, NOM_NOME, COUNT(*) AS "Repetições"
+SELECT COD_MATRICULA, NOM_NOME, COUNT(*) AS "Repetiï¿½ï¿½es"
 FROM MATRICULAS
 GROUP BY COD_MATRICULA, NOM_NOME
 HAVING COUNT(*) > 1  
@@ -73,27 +74,32 @@ GO
   GO
 
 
-
 --- SCRIPTS SOURCE
+-- fines recorded
 SELECT  COUNT(*) AS 'Autos Registrados'
 FROM autos_strans
 
+
+--  SNE
 SELECT CH_SNE, COUNT(*) AS 'Contagem'
 FROM autos_strans
 GROUP BY CH_SNE
 ORDER	BY	COUNT(*) DESC;
 
+
+-- fine's origin: radar or traffic agent
 SELECT CH_ORIGIN, COUNT(*) AS 'Contagem'
 FROM autos_strans
 GROUP BY CH_ORIGIN
 ORDER	BY	COUNT(*) DESC;
 
-
+-- car's type
 SELECT TIP_CARRO, COUNT(*) AS 'Contagem'
 FROM autos_strans
 GROUP BY TIP_CARRO
 ORDER	BY	COUNT(*) DESC;
 
+-- fine's code
 SELECT m.COD_INFRACAO , e.ST_DESCRICAO_INFRA ,  COUNT(*) AS 'Contagem'
 FROM autos_strans AS m, INFRACOES AS e
 WHERE m.COD_INFRACAO =  e.COD_INFRACAO
@@ -101,10 +107,45 @@ GROUP BY  m.COD_INFRACAO , e.ST_DESCRICAO_INFRA
 ORDER	BY	COUNT(*) DESC;
 GO
 
-  SELECT m.CH_SNE ,  MONTH(m.DT_DATA_HORA) AS 'Mês' , COUNT(*) AS 'Autos por mês'
-  FROM autos_strans AS m
-  WHERE YEAR(m.DT_DATA_HORA) = 2022
-  GROUP BY  m.CH_SNE,  MONTH(m.DT_DATA_HORA) 
-  ORDER	BY MONTH(m.DT_DATA_HORA)
-  GO
+--fine's month
+SELECT MONTH(m.DT_DATA_HORA) AS 'MÃªs' , COUNT(*) AS 'Autos por mÃªs'
+FROM autos_strans AS m
+WHERE YEAR(m.DT_DATA_HORA) = 2022
+GROUP BY  MONTH(m.DT_DATA_HORA) 
+ORDER	BY MONTH(m.DT_DATA_HORA)
+GO
 
+-- search fine by type 
+SELECT  m.NAM_ENDERECO as 'Endereï¿½o', COUNT(*) AS 'Contagem'
+FROM autos_strans AS m
+WHERE m.COD_INFRACAO = 75870
+GROUP BY m.NAM_ENDERECO
+ORDER BY 'Contagem' DESC
+GO
+
+-- search fine in specific adrress
+SELECT  m.NAM_ENDERECO as 'Endereï¿½o', COUNT(*) AS 'Contagem'
+FROM autos_strans AS m
+WHERE m.NAM_ENDERECO LIKE '%FREI %'
+
+-- search fine's adrress
+SELECT  m.NAM_ENDERECO as 'Endereï¿½o', COUNT(*) AS 'Contagem'
+FROM autos_strans AS m
+GROUP BY m.NAM_ENDERECO 
+ORDER BY 'Contagem' DESC
+GO
+
+-- search in the old db
+-- search  not paid and paid fine
+SELECT  m.status, COUNT(*)
+FROM MULTAS02 AS m
+WHERE m.ST_PLACAVEICULO != ''
+AND m.status !='NULL' AND m."status" = 'PAGA' OR m."status" = 'PENALIZADA'
+AND YEAR(m.DT_DATAINFRACAO) >= 2015 AND YEAR(m.DT_DATAINFRACAO) <= 2022
+AND m.LG_IMPRNOTIFICACAO = 'TRUE'
+GROUP BY m.status
+GO
+
+SELECT  *
+FROM INFRACOES as n
+WHERE n.ST_DESCRICAO_INFRA LIKE '%ESTA %'
